24ef77673ffe2582c9d56436b26c76c90e45f7c4
Kalenga243 <kevinkalenga10@gmail.com>
2025-08-06 02:58:55 +0200

    Sauvegarder des données en base

---
+++ src/Controller/TutoController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Tuto;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class TutoController extends AbstractController
+{
+    #[Route('/tuto', name: 'app_tuto')]
+    public function index(): Response
+    {
+        return $this->render('tuto/index.html.twig', [
+            'controller_name' => 'TutoController',
+        ]);
+    }
+
+
+    #[Route('/add-tuto', name: 'create_tuto')]
+    public function createTuto(EntityManagerInterface $entityManager): Response
+    {
+        $tuto = new Tuto();
+        $tuto->setName('Unity');
+        $tuto->setSlug('tuto-unity');
+        $tuto->setSubtitle('Lorem ipsum dolor sit amet.');
+        $tuto->setDescription('Lorem ipsum dolor sit amet.');
+        $tuto->setImage('unity.png');
+        $tuto->setVideo('DrMyhQY2udg');
+        $tuto->setLink('https://www.formation-facile.fr/formations/modelisation-denvironnements-3d-de-jeux-video-avec-blender');
+
+        // tell Doctrine you want to (eventually) save the Product (no queries yet)
+        $entityManager->persist($tuto);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        return new Response('Saved new tuto with id '.$tuto->getId());
+    }
+}
